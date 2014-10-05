335ea2b80210b096e428ad30e69f4a4e50ebef7c
Ugo Stephant <contact@dackmin.com>
2014-10-05 23:24:51 +0200

    updating Landing controller with subscribe func

--- app/scripts/controllers/landing.js
+++ app/scripts/controllers/landing.js
@@ -1,6 +1,35 @@
 'use strict';
 
-angular.module('oddlyFrontApp')
-	.controller('LandingCtrl', function ($scope) {
-		
+angular
+	.module('oddlyFrontApp')
+	.controller('LandingCtrl', function ($scope, $http, SERVER) {
+
+		/**
+		 * List of messages
+		 */
+		$scope.messages = {
+			success : "Inscription réussie",
+			error : "Une erreur est survenue",
+			already_exist : "Cet email est déjà utilisé"
+		};
+
+
+		$scope.subscribe = function(user) {
+			if($scope.subscribeForm.$valid){
+				$http({
+					method : "POST",
+					url: "http://"+ SERVER.prod.url +"/beta/account/",
+					data : user
+				})
+				.success(function(data,status,headers,config){
+					console.log(data);
+					return $scope.success = true;
+				})
+				.error(function(e,status,detail,request){
+					console.error(status);
+					console.error(request); // Request debug
+				});
+			}
+		}
+
 	});
