deb2d49241df84ea3399036be7405a867263b671
Ugo Stephant <contact@dackmin.com>
2014-10-08 23:45:12 +0200

    creating wonderful progress button

--- app/scripts/controllers/landing.js
+++ app/scripts/controllers/landing.js
@@ -15,17 +15,27 @@ angular
 
 
 		$scope.subscribe = function(user) {
+			$(".ui-progress-button").removeClass("error").removeClass("success");
+
 			if($scope.subscribeForm.$valid){
+
+				$(".progress").width((50 + Math.random() * 30) + "%")
+
 				$http({
 					method : "POST",
 					url: "http://"+ SERVER.prod.url +"/beta/account/",
 					data : user
 				})
 				.success(function(data,status,headers,config){
-					console.log(data);
+					$(".progress").width("101%");
+					$(".ui-progress-button").addClass("success");
+
 					return $scope.success = true;
 				})
 				.error(function(e,status,detail,request){
+					$(".progress").width("101%");
+					$(".ui-progress-button").addClass("error");
+
 					console.error(status);
 					console.error(request); // Request debug
 				});
