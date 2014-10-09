9626f8b6c4e52f7e6545d2050d8a488271cb6fca
Ugo Stephant <contact@ugostephant.io>
2014-10-09 17:31:49 +0200

    fix route call in app main file

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,8 @@ module.exports = function (grunt) {
 	// Time how long tasks take. Can help when optimizing build times
 	require('time-grunt')(grunt);
 
+	var modRewrite = require('connect-modrewrite');
+
 	// Configurable paths for the application
 	var appConfig = {
 		app: require('./bower.json').appPath || 'app',
@@ -110,6 +112,9 @@ module.exports = function (grunt) {
 					open: true,
 					middleware: function (connect) {
 						return [
+							modRewrite([
+								'!\\.html|\\.js|\\.css|\\.png|\\.svg|\\.jpg|\\.jpeg|\\.gif$ /index.html [L]'
+							]),
 							connect.static('.tmp'),
 							connect().use(
 								'/bower_components',
