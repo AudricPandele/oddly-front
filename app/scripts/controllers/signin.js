50fb8d81171e6bfbcd38fb95742ede0006a97907
Ugo Stephant <contact@dackmin.com>
2014-10-11 11:32:41 +0200

    shit commit

--- app/scripts/controllers/signin.js
+++ app/scripts/controllers/signin.js
@@ -4,4 +4,23 @@ angular
 	.module('oddlyFrontApp')
 	.controller('SignInCtrl', function ($scope, $http, $translate, SERVER) {
 
+		/**
+		 * @todo Faire ça plus propre
+		 * avec vérif du model user
+		 * et personnalisation message success / error
+		 */
+		$scope.register = function(user){
+			$http({
+				method : "POST",
+				url: "http://"+ SERVER.dev.url +"/api/v1/register/",
+				data : user
+			})
+			.success(function(data){
+				console.log(data);
+			})
+			.error(function(e,status,header,detail){
+				$scope.error = e.error;
+			});
+		}
+
 	});
