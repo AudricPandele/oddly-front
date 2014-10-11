26f229f309aaff5ec89064caeb4f2da5c2344233
Ugo Stephant <contact@dackmin.com>
2014-10-11 11:35:37 +0200

    fix missing colon

--- app/scripts/modules/login-check.js
+++ app/scripts/modules/login-check.js
@@ -7,7 +7,7 @@
 
 	angular.module('loginCheck', ['ngRoute','ngCookies']);
 
-	mod.directive('html', ['$rootScope','$compile','$location','$cookies'
+	mod.directive('html', ['$rootScope','$compile','$location','$cookies',
 		function($rootScope, $compile, $location, $cookies){
 			$rootScope.$on('$routeChangeStart', function (e, next, current) {
 				if(current && current.$$route && current.$$route.protected){
