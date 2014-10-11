eb3db056c4924b814021ae5d9caaffee0187e436
Ugo Stephant <contact@dackmin.com>
2014-10-11 11:37:31 +0200

    FIX FUCKING WRONG CALL TO NON EXISTING VAR

--- app/scripts/modules/login-check.js
+++ app/scripts/modules/login-check.js
@@ -5,16 +5,16 @@
 
 (function(){
 
-	angular.module('loginCheck', ['ngRoute','ngCookies']);
-
-	mod.directive('html', ['$rootScope','$compile','$location','$cookies',
-		function($rootScope, $compile, $location, $cookies){
-			$rootScope.$on('$routeChangeStart', function (e, next, current) {
-				if(current && current.$$route && current.$$route.protected){
-					console.log("protected");
-				}
-			});
-		}
-	]);
+	angular
+		.module('loginCheck', ['ngRoute','ngCookies']);
+		.directive('html', ['$rootScope','$compile','$location','$cookies',
+			function($rootScope, $compile, $location, $cookies){
+				$rootScope.$on('$routeChangeStart', function (e, next, current) {
+					if(current && current.$$route && current.$$route.protected){
+						console.log("protected");
+					}
+				});
+			}
+		]);
 
 })();
