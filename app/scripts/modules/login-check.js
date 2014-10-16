dc1632513dd7cdcbaca70f032479ff7a2b5cb9b7
Ugo Stephant <contact@dackmin.com>
2014-10-16 16:59:57 +0200

    working login check on protected routes & requests

--- app/scripts/modules/login-check.js
+++ app/scripts/modules/login-check.js
@@ -7,12 +7,38 @@
 
 angular
 	.module('loginCheck', ['ngRoute'])
-	.run(['$rootScope',function($rootScope){
+	.factory("CookieCheckator", ["$cookieStore", "$location", function($cookieStore, $location){
+		return function(){
+			if(!$cookieStore.get("sid", "username"))
+				$location.path("/signin");
+		}
+	}])
+	.factory("HttpInterceptor", ["$q", "CookieCheckator", function($q, CookieCheckator){
+		return {
+			request: function (config) {
+				if(config.checkator)
+					CookieCheckator();
 
+				return config || $q.when(config);
+			},
+
+			requestError: function(rejection) { return $q.reject(rejection); },
+			response: function(response){ return response || $q.when(response); },
+			responseError: function(rejection){ return $q.reject(rejection); }
+		}
+	}])
+	.config(function($httpProvider){
+
+		//Add check on http request
+		$httpProvider.interceptors.push('HttpInterceptor');
+
+	})
+	.run(["$rootScope", "CookieCheckator", function($rootScope, CookieCheckator){
+
+		//Add check on route change
 		$rootScope.$on('$routeChangeStart', function (e, next, current) {
-			if(next && next.$$route && next.$$route.protected){
-				console.log("protected route")
-			}
+			if(next && next.$$route && next.$$route.checkator)
+				CookieCheckator();
 		});
 
 	}]);
