fa001f18bc8559d614a0db64f4e54752ba4c6022
Ugo Stephant <contact@dackmin.com>
2014-10-26 19:25:37 +0100

    create general app controller

---
+++ app/scripts/controllers/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular
+	.module('oddlyFrontApp')
+	.controller('GeneralAppCtrl', function ($scope, $http, $location, $translate, SERVER) {
+
+		//Check current route for active links
+		$scope.activeRoute = function(rt){
+			return rt === $location.path();
+		};
+
+
+		//Change lang
+		$scope.setLocale = function(name, code, locale){
+			$translate.use(locale);
+			$scope.selected = { name: name, code: code };
+		};
+
+	});
