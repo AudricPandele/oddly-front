6b110409bfae265665c0b6dfee2c75fe91733c95
Ugo Stephant <contact@dackmin.com>
2015-01-09 19:02:18 +0100

    update artist controller

--- app/scripts/controllers/artist.js
+++ app/scripts/controllers/artist.js
@@ -5,6 +5,10 @@ angular
 
 	.controller('ArtistCtrl', function ($scope, $http, $location, $translate, SERVER, $routeParams) {
 		$scope.SERVER = SERVER;
+		$scope.types = [
+			"Writer",
+			"Drawer"
+		];
 
 		// Get artist infos
 		$http({
@@ -13,10 +17,26 @@ angular
 			checkator: true
 		})
 		.success(function(data){
+			console.log(data.artist);
 			$scope.artist = data.artist;
 		})
 		.error(function(e){
 			console.log(e);
 		});
+
+
+		$scope.getRank = function(rate){
+			rate = parseInt(rate);
+			rate = (rate > 5) ? 5 : ((rate < 0) ? 0 : rate);
+
+			var full_stars = rate;
+			var empty_stars = 5 - rate;
+
+			var res = "";
+			for(var i = 0; i < full_stars; i++) res+="<i class='fa fa-star'></i>";
+			for(var i = 0; i < empty_stars; i++) res+="<i class='fa fa-star-o'></i>";
+
+			return res;
+		};
 
 	})
