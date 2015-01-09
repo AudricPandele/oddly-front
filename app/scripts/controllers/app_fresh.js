55227830af30f3c8326be19503e9966320ac364f
Ugo Stephant <contact@dackmin.com>
2015-01-09 11:10:16 +0100

    fix app_fresh_type controller & view

--- app/scripts/controllers/app_fresh.js
+++ app/scripts/controllers/app_fresh.js
@@ -6,6 +6,24 @@ angular
 	//General app controller
 	.controller('FreshTypeCtrl', function ($scope, $http, $location, $translate, SERVER, $routeParams, $rootScope) {
 		$scope.type = $routeParams.type.charAt(0).toUpperCase() + $routeParams.type.slice(1);
+		$scope.SERVER = SERVER;
+		$scope.types = {
+			"books" : 0,
+			"comics" : 1,
+			"magazines" : 2,
+			"newspapers" : 3
+		};
 
-		
+
+		$http({
+			method: "GET",
+			url: SERVER.METHOD + SERVER.API + "/items/fresh/"+$scope.types[$routeParams.type],
+			checkator: true
+		})
+		.success(function(data){
+			$scope.items = data.items;
+		})
+		.error(function(e){
+			console.log(e);
+		})
 	})
