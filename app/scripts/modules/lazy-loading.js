2924cdfe523d4168e6bd7f1aec79528972dbd6aa
bltnico <bellot.n@gmail.com>
2015-01-09 18:40:59 +0100

    :angel:

--- app/scripts/modules/lazy-loading.js
+++ app/scripts/modules/lazy-loading.js
@@ -1,40 +1,73 @@
 'use strict'
 
-angular.module('oddlyFrontApp').service('$lazyloading', function(SERVER) {
+/**
+ * $lazyloading servie
+ *
+ * @param (object)
+ */
+angular
+	.module('oddlyFrontApp')
+	.service('$lazyloading', function(SERVER) {
 
-	this.img = {};
-	this.url = "";
-	this.total_pages;
+		/**
+		 * @var (object)
+		 */
+		this.img = {};
 
-	this.init = function(id, total_pages, page, quality) {
-		page = page || 1;
-		quality = quality || "SD";
+		/*
+		 * @var (string)
+		 */
+		this.url = "";
 
-		this.url = SERVER.METHOD + SERVER.CDN + "/item/" + id + "/" + quality + "/";
-		this.total_pages = total_pages;
+		/**
+		 * @var (int)
+		 */
+		this.total_pages;
 
-		return this.stream(page);
-	}
+		/**
+		 * Init lazy loading
+		 *
+		 * @param (string)
+		 * @param (int)
+		 * @param (int)
+		 * @param (string)
+		 * @return (function) stream()
+		 */
+		this.init = function(id, total_pages, page, quality) {
+			page = page || 1;
+			quality = quality || "SD";
 
-	this.stream = function(page) {
-		var page = parseInt(page);
-		if(this.img.hasOwnProperty(page)) {
-			var item = new Image();
-			item.src = this.url + page;
-			this.img[page] = item;
+			this.url = SERVER.METHOD + SERVER.CDN + "/item/" + id + "/" + quality + "/";
+			this.total_pages = total_pages;
 
-			var itemLz = new Image();
-			itemLz.src = this.url + parseInt(page + 1);
-			this.img[parseInt(page + 1)] = itemLz;
-
-			return item;
-		} else {
-			var item = new Image();
-			item.src = this.url + page;
-			this.img[page] = item;
-
-			return item;
+			return this.stream(page);
 		}
-	}
 
-});
+		/**
+		 * Stream image
+		 *
+		 * @param (int) 
+		 * @return (object)
+		 */
+		this.stream = function(page) {
+			var page = parseInt(page);
+			if(this.img.hasOwnProperty(page)) {
+				var item = new Image();
+				item.src = this.url + page;
+				this.img[page] = item;
+
+				var itemLz = new Image();
+				itemLz.src = this.url + parseInt(page + 1);
+				this.img[parseInt(page + 1)] = itemLz;
+
+				return item;
+			} else {
+				var item = new Image();
+				item.src = this.url + page;
+				this.img[page] = item;
+
+				return item;
+			}
+		}
+
+	});
