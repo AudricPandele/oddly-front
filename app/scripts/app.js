86ebb02f31896f35199e5c70a771ca0f1e178361
Ugo Stephant <contact@dackmin.com>
2015-01-16 17:03:58 +0100

    add get current route method

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -69,7 +69,7 @@ angular
 	})
 
 
-	.run(function($cookieStore, $translate, $rootScope, $location){
+	.run(function($cookieStore, $translate, $rootScope, $location, $route){
 
 		//Try to set locale from cookie
 		$translate.use($cookieStore.get("locale"));
@@ -86,6 +86,11 @@ angular
 		$rootScope.back = function () {
 			var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/app";
 			$location.path(prevUrl);
+		};
+
+
+		$rootScope.getCurrentRoute = function(route){
+			return route == $route.current.$$route.originalPath;
 		};
 
 	})
