71fb340b0e44b21dc011ad16ddff2a1296b397ed
Ugo Stephant <contact@dackmin.com>
2015-01-23 11:34:25 +0100

    add last read item to menu

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -3,6 +3,17 @@
 angular
 	.module('oddlyFrontApp')
 
-	.controller('MenuCtrl', function($scope, $routeParams) {
+	.controller('MenuCtrl', function($scope, $routeParams, SERVER, $http) {
 		$scope.routeParams = $routeParams;
+		$scope.SERVER = SERVER;
+
+		$http({
+			method: "GET",
+			url: SERVER.METHOD + SERVER.API + "/bookshelf/last",
+			checkator: true
+		})
+		.success(function(data){
+			$scope.item = data.item;
+		})
+
 	})
